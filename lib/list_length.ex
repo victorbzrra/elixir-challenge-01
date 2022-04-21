84158ca60339a4914335985eb578cfd62cef872b
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-04-21 00:11:50 -0300

    Challenge solved

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list, 0)
+
+  defp length([], acc) do
+    acc
+  end
+
+  defp length([_head | tail], acc) do
+    length(tail, acc + 1)
   end
 end
